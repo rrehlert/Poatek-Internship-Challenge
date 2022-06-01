7d514f4fc074de7fad44c4ec0e100c3392610b14
rrehlert <ricardo.r.e.2000@gmail.com>
2022-06-01 20:44:28 -0300

    removed comments from Classes.py script

--- Programming/Classes.py
+++ Programming/Classes.py
@@ -6,57 +6,43 @@ class FuelPump:
     def setPrice(self, price):
         if price >= 0:
             self.__fuelPrice = price
-            #print(f"Price set at {self.__fuelPrice}")       #Debug purposes
             return price
-        else:
-            #print(f"Price cannot be negative")       #Debug purposes   
+        else:  
             return 0
         
 
     def get_price(self):
-        #print(f"Price is {self.__fuelPrice}")       #Debug purposes
         return self.__fuelPrice        
 
     def setQuantity(self, quantity):
         if quantity >= 0:
             self.__fuelQuantity = quantity
             return quantity
-            #print(f"Liters available {self.__fuelQuantity}")        #Debug purposes
-        else:
-            #print(f"Quantity cannot be negative")       #Debug purposes       
+        else:     
             return 0
 
     def get_quantity(self):
-        #print(f"Liters available {self.__fuelQuantity}")        #Debug purposes
         return self.__fuelQuantity        
 
     def check_fuel_by_price(self, price):
         liters = price / self.get_price()
         liters_available = self.get_quantity()
-        #print(f"Price requested: {price}, Liters wanted: {liters}, Liters available: {liters_available} ")        #Debug purposes
         if liters_available >= liters:
-            #print("Check OK")       #Debug purposes
             return liters    
         else:
-            #print("Check not OK")       #Debug purposes
             return 0
             
 
     def check_fuel_by_liters(self, liters):
         liters_available = self.get_quantity()
-        #print(f"Liters wanted: {liters}, Liters available: {liters_available}")     #Debug purposes
         if liters_available >= liters:
             price = liters * self.get_price()
-            #print("Check OK")       #Debug purposes
             return price
         else:
-            #print("Check not OK")       #Debug purposes
             return 0
 
     def __pump_liters(self, liters):
         self.__fuelQuantity = self.__fuelQuantity - liters
-        #print(f"Liters pumped {liters}")        #Debug purposes
-        #print(f"Liters available {self.__fuelQuantity}")        #Debug purposes
 
     def fillWithPrice(self, price):
         if not price or price < 0:
